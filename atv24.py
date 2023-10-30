82ca2e836cdcb02ebe2da2d305aae5a1c17c48ba
sofymiranda <mirandaspfya@gmail.com>
2023-10-30 14:20:52 -0300

    atividade 24

---
+++ atv24.py
@@ -0,0 +1,8 @@
+# Exercício Python 24: Refaça a tabuada de um número
+# que o usuário escolher, só que agora utilizando um laço for.
+num=int (input("Insira o número")) 
+for tabu in range (11):
+    result= tabu * num
+    print (result )
+
+
